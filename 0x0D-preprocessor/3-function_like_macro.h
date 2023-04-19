110428b47ecc8ea85f435ba610fec4a27380f261
ligh-cloud <belghitihatim00@gmail.com>
2023-04-18 19:50:20 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
+
+
+
+======================================
+
+4-sum.h CODE
+
+
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
